0a985af0e301957fe22d498370f1dd5a572e057b
Ramazan Atalay <ramazanatalay@gmail.com>
2022-01-04 22:34:42 -0500

    Modified

--- ec2-status-checks.py
+++ ec2-status-checks.py
@@ -1,8 +1,8 @@
 import boto3
 import schedule
 
-ec2_client = boto3.client('ec2', region_name="eu-central-1")
-ec2_resource = boto3.resource('ec2', region_name="eu-central-1")
+ec2_client = boto3.client('ec2', region_name="us-east-1")
+ec2_resource = boto3.resource('ec2', region_name="us-east-1")
 
 
 def check_instance_status():
